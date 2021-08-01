ac7dd651dc2511fa49d28f084e9e6897c1771ccc
nik-ves <nik-ves@hotmail.com>
2021-08-01 17:25:14 +0200

    Reset game logic

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = () => {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+  
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+  
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = () => {
     const currentPlayer = document.querySelector(`#current--${activePlayer}`);
@@ -28,12 +41,9 @@ const switchPlayer = () => {
 
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
-}
+};
 
-const roll = () => {
-    // generate random dice roll
-    // display dice
-    // check for rolled 1: if true, switch to next player
+const rollDice = () => {
 
     if (playing) {
         const dice = Math.trunc(Math.random() * 6) + 1;
@@ -48,13 +58,10 @@ const roll = () => {
         } else {
             switchPlayer();
         };
-    }
+    };
 };
 
-const hold = () => {
-    // add current score to active players score
-    // check if players score is >= 100
-    // finish game and if not switch to other player
+const holdScore = () => {
 
     if (playing) {
         scores[activePlayer] += currentScore;
@@ -62,7 +69,7 @@ const hold = () => {
         const playerScore = document.querySelector(`#score--${activePlayer}`);
         playerScore.textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             playing = false;
             diceEl.classList.add('hidden');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -73,5 +80,6 @@ const hold = () => {
     };
 };
 
-btnRoll.addEventListener('click', roll);
-btnHold.addEventListener('click', hold);
+btnRoll.addEventListener('click', rollDice);
+btnHold.addEventListener('click', holdScore);
+btnNew.addEventListener('click', init);
