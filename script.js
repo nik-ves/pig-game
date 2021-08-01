984b04a0cc7daca163919f6f1033f92e1d440606
nik-ves <nik-ves@hotmail.com>
2021-08-01 16:10:55 +0200

    Starting with the game logic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// JS
+const playerOneScore = document.querySelector('#score--0');
+const playerTwoScore = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+dice.classList.add('hidden');
