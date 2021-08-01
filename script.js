12abbaa8a7f7e374728eb36c31760d61415b9ce9
nik-ves <nik-ves@hotmail.com>
2021-08-01 16:38:42 +0200

    Switching the active player

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -12,7 +14,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 const roll = () => {
     // generate random dice roll
@@ -26,10 +30,16 @@ const roll = () => {
     diceEl.src = `img/dice-${dice}.png`;
 
     if (dice !== 1) {
+        const currentPlayer = document.querySelector(`#current--${activePlayer}`);
         currentScore += dice;
-        current0El.textContent = currentScore; // later will be changed to correct player
+        currentPlayer.textContent = currentScore;
     } else {
-        
+        const currentPlayer = document.querySelector(`#current--${activePlayer}`);
+        currentPlayer.textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     };
 };
 
